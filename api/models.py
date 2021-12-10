e08f7103cafab0238270aa27f5db3c48cd7adb8f
Zeroin9 <dol00758@gmail.com>
2021-12-10 06:39:50 +0000

    added model for Parcel

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Parcel(models.Model):
+    track_code = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.track_code)
