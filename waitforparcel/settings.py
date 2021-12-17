f6efdd1ce4f543c713c1d20267af8a93c104f226
Zeroin9 <dol00758@gmail.com>
2021-12-17 15:45:44 +0800

    change settings for local

--- waitforparcel/settings.py
+++ waitforparcel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^8rvvka(m!5g@%bu5aeo@+m&3(whaxe=e2_=2q5*mew2eohmqt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ishchi.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
